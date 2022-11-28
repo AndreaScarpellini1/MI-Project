3e2d0a2d7be3cf61d4a111c06e77b0f694f56248
AndreaScarpellini1 <91903540+AndreaScarpellini1@users.noreply.github.com>
2022-11-28 17:05:01 +0100

    Update Project.m

--- Project.m
+++ Project.m
@@ -1,66 +1,53 @@
 clc
 clear
 close all 
+cd 'C:\Users\scrpa\OneDrive - Politecnico di Milano\Desktop\Poli\Magistrale\Primo anno\BSPMI\MI\project repo\MI-Project'
 %% Animation on: a=1 Animation off: a=0;
-a=0;
+a=1;
 %% Visualizzazione dei dati 
 load MRIdata.mat
-cmap = colormap('jet');
 
 if (a==1)
-    figure()
-    for i=1:length(vol(1,1,:))
-        subplot(2,1,1)
-           imshow(vol(:,:,i))
-           colorbar
-           title("MRI iniziale.")
-        subplot(2,1,2)
-           imshow(vol(:,:,i),'Colormap', cmap)
-           colorbar
-           title("MRI colored")
-           pause(0.01)
-    end 
-end
-
-figure()
-imshow(vol(:,:,90))
-
-%% Binarize image
-for i=1:length(vol(1,1,:))
-    bin_vol(:,:,i) = imbinarize(vol(:,:,i));
-end
-%%
-figure()
-for i=1:length(vol(1,1,:))
-    imshow(bin_vol(:,:,i))
-    pause(0.01)
-end
-%%
-figure()
-imshow(fant_pet_bin)
-% Binarize the image using as threshold half of the maximum
-%%
-imcontour(fant_pet_bin, 1, 'm');
-
-%%
-figure()
-for i=1:length(vol(1,1,:))
-       imshow(vol_cont(:,:,i))
-       title("MRI binarized.")
-       pause(0.01)
-end
-
-%% 1. Briefly review the topic of tissue and lesion segmentation over MRI images
+    figure(1)
+        montage(vol)
+        title("MRI iniziale.")
+    figure(2)
+        montage(vol(:,:,64:90))
+        title('Tumor From slice 64 to 90.')
+end 
 
 
-%% 2. Implement a workflow, motivating its main sub-steps, to pre-process an MRI image and segment a lesion based on methods we have seen during lessons.
-%SEGMENTATION:
+%% SEGMENTATION:
 %The goal of image segmentation is to divide an image into a set of 
 % semantically meaningful,homogeneous,nonoverlapping 
 % regions of similar attributes such as intensity, depth, color, or
 % texture. The segmentation result is either an image of labels identifying each 
 % homogeneous region or a set ofcontours which describe the region boundaries.
 
+figure()
+subplot(2,1,1)
+imshow(vol(:,:,75))
+impixelinfo
+title("Isolate the tumor")
+subplot(2,1,2)
+[Cropped_vol d]= imcrop(vol(:,:,75));
+
+% Dimensioni del taglio 
+v1=round(d(2)):(round(d(2))+length(Cropped_vol(:,1)));
+v2=round(d(1)):(round(d(1))+length(Cropped_vol(1,:)));
+v3=64:90;
+
+figure()
+subplot(2,1,1)
+imshow(vol(v1,v2,75));
+title('Immagine ricavata dalle dimensioni')
+subplot(2,1,2)
+title('Immagine ricavata dalla funzione')
+imshow(Cropped_vol)
+
+%3D Visualization 
+volumeViewer(vol(v1,v2,v3))
+%%
 
 %% 3. Segment the lesion and calculate the respective cross-sectional area over sagittal slice number 135 4. Identify sagittal slices that contain the lesion and extend the quantification of its cross-sectional area to the whole volume. Try to repeat this process across axial slices. What are the main challenges of segmenting this lesion with respect to other cerebral tissues and orthogonal views?
 
