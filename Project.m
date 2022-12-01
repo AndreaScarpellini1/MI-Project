3c8baca12a6b4d277e6f53afdfb3bdad126cad6d
AndreaScarpellini1 <91903540+AndreaScarpellini1@users.noreply.github.com>
2022-12-01 13:18:34 +0100

    Update Project.m

--- Project.m
+++ Project.m
@@ -1,7 +1,7 @@
 clc
 clear
 close all 
-%cd 'C:\Users\scrpa\OneDrive - Politecnico di Milano\Desktop\Poli\Magistrale\Primo anno\BSPMI\MI\project repo\MI-Project'
+cd 'C:\Users\scrpa\OneDrive - Politecnico di Milano\Desktop\Poli\Magistrale\Primo anno\BSPMI\MI\project repo\MI-Project'
 %% Animation on: a=1 Animation off: a=0;
 a=1;
 %% dati 
@@ -29,46 +29,112 @@ v1=round(d(2)):(round(d(2))+length(Cropped_vol(:,1)));
 v2=round(d(1)):(round(d(1))+length(Cropped_vol(1,:)));
 v3=64:90;
 
-figure()
+figure('Name',"Confronto funzione matlab e dimensioni ottenute")
 subplot(2,1,1)
 imshow(vol(v1,v2,75));
 title('Immagine ricavata dalle dimensioni')
 subplot(2,1,2)
-title('Immagine ricavata dalla funzione')
 imshow(Cropped_vol)
+title('Immagine ricavata dalla funzione')
 
-%Aumento del contrasto
-j=0;
-for i=v3
-    j=j+1;
-    vol_imadjust(:,:,j) = imadjust(vol(v1,v2,i));
+%% studio histogrammi 
+VOI=vol(v1,v2,v3);
+figure('Name', "Istogrammi")
+for i=1:27
+    subplot(2,1,1)
+    imshow(VOI(:,:,i))
+    colorbar
+    subplot(2,1,2)
+    histogram(VOI(:,:,i),255); 
+    xlim([0,255])
+    grid on 
+    pause(1)
 end 
 
+%%
+%Aumento del contrasto
+gamma=[0.5,1,1.5,2];
+gammas={'0.5','1.0','1.5','2.0'};
+figure()
+
+LOWin = 150/255;
+HIGHin = max(max(VOI(:,:,i)))/255;
+
+for z=1:length(gamma)
+   
+    for i=1:size(VOI,3)
+        vol_imadjusted(:,:,i) = imadjust(VOI(:,:,i),[0 0.5882],[0 1],gamma(z));
+    end 
+    subplot(1,length(gamma),z)
+    montage(vol_imadjusted)
+    title(['Gamma =' gammas(z)])
+end 
+% gamma --> 2 
+
+
+figure('Name', "Istogrammi")
+for i=1:27
+    subplot(2,2,1)
+        imshow(vol_imadjusted(:,:,i))
+        colorbar
+        title("Modified image")
+    subplot(2,2,2)
+        histogram(vol_imadjusted(:,:,i),255); 
+        xlim([0,255])
+        grid on 
+    subplot(2,2,3)
+        imshow(VOI(:,:,i))
+        colorbar
+    subplot(2,2,4)
+        histogram(VOI(:,:,i),255); 
+        xlim([0,255])
+        xline(150,'r')
+        grid on 
+        title("Original image")
+    pause(1)
+end 
+
+
+%%
 %salt & pepper filtering
 for i=1:length(v3)
-    vol_imadjust(:,:,i)=medfilt2(vol_imadjust(:,:,i), [5 5]);
+    vol_pn(:,:,i)=medfilt2(vol_imadjusted(:,:,i), [6 6]);
 end
 
+figure()
+montage(vol_pn)
+%%
 %Binarizzazione 
-bin_vol=imbinarize(vol_imadjust,0.4);
+bin_vol=imbinarize(vol_pn,0.8);
 
 figure()
 subplot(1,2,1)
-montage(vol_imadjust)
-title('Enhanced contrast')
+montage(vol_pn)
+title('BEFORE BIN')
 subplot(1,2,2)
 montage(bin_vol)
+title("AFTER BIN")
 
-%Prendo i contorni 
+%% Prendo i contorni 
+
+vidfile = VideoWriter('testmovie.mp4','MPEG-4');
+open(vidfile);
 figure()
+j=0;
 for i=2:26
-    imshow(bin_vol(:,:,i))
+    j=j+1;
+    imshow(VOI(:,:,i))
+    title("Contour of the tumor")
     hold on
-    imcontour(bin_vol(:,:,i),3,'m')
+    imcontour(bin_vol(:,:,i),5,'m');
+    F(j) = getframe(gcf); 
+    writeVideo(vidfile,F(j));
     pause (1)
 end 
-title("Contours of the tumor")
+close(vidfile)
 
+
+%%
 % area of the binarized image 
 Axial_num_pixel=0;
 for i=2:26
@@ -134,7 +200,7 @@ figure()
 for i=2:26
     imshow(bin_vol(:,:,i))
     hold on
-    imcontour(bin_vol(:,:,i),3,'m')
+    imcontour(bin_vol(:,:,i),4,'m')
     pause (1)
 end 
 title("Contours of the tumor")
